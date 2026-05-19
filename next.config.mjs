d18c1f2c48c818ee27b62c8010b14e69e611771f
harshrao26 <harshurao058@gmail.com>
2026-05-19 17:53:42 +0530

    feat: add rewrite rule to map /thank-you.html to /thank-you path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/thank-you.html",
+        destination: "/thank-you",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
